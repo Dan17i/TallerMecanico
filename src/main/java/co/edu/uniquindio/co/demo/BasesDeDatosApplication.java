d72f783dbb4457257438272a2542bba0ed0574f7
Daniel Jurado <139478928+Dan17i@users.noreply.github.com>
2025-11-06 18:26:03 -0500

    cuerpo del proyecto
    este es el cuerpo del proyecto de bases de datos 1

---
+++ src/main/java/co/edu/uniquindio/co/demo/BasesDeDatosApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.co.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasesDeDatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasesDeDatosApplication.class, args);
+	}
+
+}
